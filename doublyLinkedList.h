c5581d5fa135fca1ac7abfa684310bef97f26e9f
GPCecchetti <GPCecchetti@gmail.com>
2023-02-06 14:03:34 +0100

    weitere kleine Änderungen

--- doublyLinkedList.h
+++ doublyLinkedList.h
@@ -15,29 +15,66 @@
 #include <Windows.h>
 
 typedef struct knot knot_t;
-typedef struct dcList dcList_t;
+typedef struct dlList dlList_t;
 
+// Konstruktor
 /// <summary>
 /// Initializes a double chained list
 /// </summary>
 /// <param name="list">List to be initialized</param>
 /// <returns>0 if initialized successfully, otherwise use DCLGetLastError() to retreive Errorcode</returns>
-int DLL_Init(dcList_t**);
+int DLList_Init(dcList_t**);
+
+// Destruktor
+/// <summary>
+/// frees the allocated space of the list and all contained elements
+/// </summary>
+/// <param name="list">List to be destroyed</param>
+/// <returns>0 if initialized successfully, otherwise use DCLGetLastError() to retreive Errorcode</returns>
+int DLList_Destroy(dcList**);
 
 /// <summary>
 /// Counts the elements contained in a list
 /// </summary>
-/// <param name=""></param>
+/// <param name="list"></param>
 /// <returns></returns>
-int DLL_Count(dcList_t**, int*);
-int SetCursorToBegin(dcList_t**);
-int SetCursorToEnd(dcList_t**);
+int DLList_Count(dlList_t*);
+int DLList_GetLength(dlList_t*);
+
+// Cursor Operations to move through the list
+/// <summary>
+/// Set the cursor to the previous element of the list
+/// </summary>
+/// <param name="list">List on which the operations should be performed</param>
+/// <returns>0 if cursor was moved successfully, otherwise use DCLGetLastError() to retreive Errorcode</returns>
+int DLList_Prev(dlList_t*);
+/// <summary>
+/// Set the cursor to the next element of the list
+/// </summary>
+/// <param name="list">List on which the operations should be performed</param>
+/// <returns>0 if cursor was moved successfully, otherwise use DCLGetLastError() to retreive Errorcode</returns>
+int DLList_Next(dlList_t*);
+/// <summary>
+/// Set the cursor to the first element of the list
+/// </summary>
+/// <param name="list">List on which the operations should be performed</param>
+/// <returns>0 if cursor was moved successfully, otherwise use DCLGetLastError() to retreive Errorcode</returns>
+int DLList_SetCursorToBegin(dlList_t*);
+/// <summary>
+/// Set the cursor to the last element of the list
+/// </summary>
+/// <param name="list">List on which the operations should be performed</param>
+/// <returns>0 if cursor was moved successfully, otherwise use DCLGetLastError() to retreive Errorcode</returns>
+int DLList_SetCursorToEnd(dlList_t*);
+
+
 int GetData(dcList_t*, void**);
 int DCL_GetDataFromIndex(dcList_t**, void*, int);
 int DCL_InsertBefore(dcList_t*, void**);
 int DCL_InsertAfter(dcList_t*, void**);
 int DCL_GetLength(dcList_t*);
 
+// Error Handling
 int DCL_GetLastError(void);
 
 /// <summary>
