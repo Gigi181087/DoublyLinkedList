5e1c065ec9cd0187b4612ca81eb836256a745bbd
Gian Piero Cecchetti <gpcecchetti@gmail.com>
2023-02-11 15:26:16 +0100

    implemented DLList_InternalSwitchNodes

--- doublyLinkedList_Intern.c
+++ doublyLinkedList_Intern.c
@@ -17,3 +17,28 @@ void DLList_MoveAfter(knot_t* knot) {
 
     return;
 }
+
+void DLList_MoveToIndex(knot* knot, int index) {
+
+    return;
+}
+
+void DLList_Internal_SwitchNodes(node_t* node1, node_t* node2) {
+    node_t buff;
+    node1->prev->next = &buff;
+    node1->next->prev = &buff;
+    buff.next = node1->next;
+    buff.prev = node1->prev;
+
+    node2->prev->next = node1;
+    node2->next->prev = node1;
+    node1->prev = node2->prev;
+    node1->next = node2->next;
+
+    node2->prev = buff.prev;
+    node2->next = buff.next;
+    node2->prev->next = ndoe2;
+    node2->next->prev = node2;
+
+    return;
+}
